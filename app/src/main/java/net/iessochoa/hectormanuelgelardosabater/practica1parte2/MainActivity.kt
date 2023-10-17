f0194293e6b83830c8921088abc430593c2ade8b
Hector <hectormanuel.gs@hotmail.com>
2023-10-17 17:11:14 +0200

    Paso 1 Parte 2 falta comprobar la contraseña

--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica1parte2/MainActivity.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica1parte2/MainActivity.kt
@@ -2,10 +2,35 @@ package net.iessochoa.hectormanuelgelardosabater.practica1parte2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNumero: EditText
+    private lateinit var btComprobar: Button
+
+    fun enviarResultado() {
+//comprobamos si el campo está vacío
+        if (!etNumero.text.isEmpty()) {
+//Creamos el Intent hacia resultadoActivity
+            val intent = Intent(this, resultadoActivity::class.java)
+//le pasamos el contenido como clave:valor(parecido a un map)
+            intent.putExtra(resultadoActivity.EXTRA_NUMERO, etNumero.text.toString())
+//iniciamos la activity resultadoActivity
+            startActivity(intent)
+        } else
+//enviamos un mensaje por pantalla
+            Toast.makeText(this, getString(R.string.msj_numero), Toast.LENGTH_LONG).show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etNumero = findViewById(R.id.etNumero)
+        btComprobar = findViewById(R.id.btComprobar)
+        //asignamos acción como lambda
+        btComprobar.setOnClickListener{enviarResultado()}
     }
 }
--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica1parte2/resultadoActivity.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica1parte2/resultadoActivity.kt
@@ -2,10 +2,23 @@ package net.iessochoa.hectormanuelgelardosabater.practica1parte2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class resultadoActivity : AppCompatActivity() {
+    companion object {
+        val EXTRA_NUMERO = "Practica1Parte2.resultadoActivity.numero"
+    }
+
+    private lateinit var tvResultado: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+        //asignamos la instancia de tvResultado en onCreate
+        tvResultado=findViewById(R.id.tvResultado)
+        //recuperamos el valor enviado por MainActivity
+        val numero=intent.getStringExtra(EXTRA_NUMERO)
+        tvResultado.text= String.format(getString(R.string.resultado),numero)
     }
+
+
 }
